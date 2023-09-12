7726ba111ec08e819a1a6abaaa4778d827bb26dd
Enes Yesil <me.enesyesil@gmail.com>
2023-09-12 13:50:07 -0400

    v.103

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -9,33 +9,29 @@ const HomePage = () => {
 
 return(
   
-    <div className=" bg-gradient-to-r from-blue-400 to-purple-500 min-h-screen flex items-start justify-center">
-       <nav class="bg-white border-gray-200 dark:bg-gray-900 w-full fixed ">
-       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"> 
-       <div class="flex md:order-2">
-  </div>
-  <div class="items-start justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <div className=" bg-slate-700 min-h-screen flex flex-col items-center justify-center space-y-4">
+      <div className=' flex flex-col items-start justify-center space-y-4'>
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Decisions.</span> Fisor.</h1>
+      <p class="mb-4 text-lg font-normal text-white lg:text-xl justify-center">Let Fisor guide you through the process.</p>
+      <p class="mb-4 text-lg font-normal text-white lg:text-xl justify-center space-y-4">Calculate the present value, estimate maintenance costs, and compare vehicles—all while staying within your budget.</p>
+      </div>
+      <span></span>
 
-    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <Link to="/home" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
-      </li>
-      <li>
-        <Link to="/car"  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Car</Link>
-      </li>
-      <li>
-        <Link to="/Housing" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Housing</Link>
-      </li>
-      <li>
-        <Link to="/InvestmentPlan" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Investment</Link>
-      </li>
-    </ul>
+    <div className='flex flex-col items-center justify-center '>    
+      <button type="button"
+               to="/car"
+           
+           className="bg-gradient-to-r to-emerald-600 from-sky-400 text-white px-8 py-4 rounded hover:bg-blue-600 w-full transition duration-300">
+              <Link to="/car"> Get Started</Link>
+      </button>
+       
+      </div>  
+
+   
+
   </div>
        
-       </div>
-
-       </nav>
-    </div>
+       
     
 )
 
